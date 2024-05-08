7e48656a6a4579bac9b9790e7204a7d7a0dea165
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-08 13:51:05 +0000

    import ex3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,10 @@
 # Exercise 3
 # Your solution comes here
+
+n = int(input("How many seconds have passed throughout the day?: "))
+
+hours = (n // 3600) % 24
+minutes = (n % 3600) // 60
+seconds = n % 60
+
+print(f"{hours}:{minutes:02d}:{seconds:02d}")
