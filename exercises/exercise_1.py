cce6c19e71d4aed3ee96baabcfa56139e9363843
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-08 13:27:24 +0000

    import ex1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,9 @@
 # Exercise 1
 # Your solution comes here
+
+num = int(input("Input a 5 digit integer: "))
+num1 = str(((num // 10000) % 10) + ((num // 100) % 10) + ((num // 1) % 10))
+num2 = str(((num // 1000) % 10) + ((num // 10) % 10))
+answer = num1 + num2
+print(answer)
+
