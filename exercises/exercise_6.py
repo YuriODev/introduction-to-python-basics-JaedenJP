5fdf6bc91f1e3ecd5490cea766efed14841424b6
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-08 14:04:36 +0000

    import ex6

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,10 @@
 # Exercise 6
 # Your solution comes here
+
+a = int(input("Input an integer: "))
+b = int(input("Input another integer: "))
+
+if a % b == 0:
+    print("YES")
+else:
+    print("NO")
