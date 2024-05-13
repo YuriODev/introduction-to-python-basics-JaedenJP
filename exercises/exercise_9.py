dc8fe787dd431d53ffcecc287e1b0ffcb063339b
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-13 10:52:05 +0000

    import ex9

--- exercises/exercise_9.py
+++ exercises/exercise_9.py
@@ -1,2 +1,12 @@
 # Exercise 9
 # Your solution comes here
+
+h = int(input("How many hours have passed?: "))
+m = int(input("How many minutes have passed?: "))
+s = int(input("How many seconds have passed?: "))
+
+hours = h * 30
+minutes = m * 0.5
+seconds = s * 0.1
+
+print(f"Angle is: {hours + minutes + seconds} degrees")
