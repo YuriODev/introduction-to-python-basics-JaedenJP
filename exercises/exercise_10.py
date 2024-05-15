eca679f7a136f081d19e58e84b747cda310a66e2
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-15 13:23:09 +0000

    import ex10

--- exercises/exercise_10.py
+++ exercises/exercise_10.py
@@ -1,2 +1,8 @@
 # Exercise 10
 # Your solution comes here
+
+a = float(input("Angle of hour hand since beginning of the day?: "))
+
+angle = (a % 30) * 12
+
+print(f"Angle of minute hand since beginning of the day is:{angle}")
