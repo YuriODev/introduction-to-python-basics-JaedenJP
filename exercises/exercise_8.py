e9812f1d6747b2950e1377e701ee99bb384bf58a
JaedenJP <168652531+JaedenJP@users.noreply.github.com>
2024-05-17 08:20:46 +0000

    import ex8

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,14 @@
 # Exercise 8
 # Your solution comes here
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+max_value = max(n1, n2, n3)
+min_value = min(n1, n2, n3)
+mid = (n1 + n2 + n3) - max_value - min_value
+
+print(min_value)
+print(mid)
+print(max_value)
